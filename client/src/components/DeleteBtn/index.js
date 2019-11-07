400167b4af09aa423202998cd3c6b0d9262c2e84
Tierra <sr.undershaft@gmail.com>
2019-11-07 01:24:04 -0600

    added inline styles to fix heroku issue

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -4,8 +4,15 @@ import "./style.css";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
+  const style = {
+      color: "#e74944",
+      position: "absolute",
+      bottom: "1.5rem",
+      right: "3rem",
+      margin: "0.6rem"
+    }
   return (
-    <button type="button" className="delete-btn btn btn-outline-dark"  {...props} tabIndex="0">
+    <button style={style} type="button" className="delete-btn btn btn-outline-dark"  {...props} tabIndex="0">
       Remove
     </button>
   );
